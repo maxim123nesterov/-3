3d530cc270b7ec36249aedd9f53208ecfbbcbd3b
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2023-01-19 23:07:27 +0300

    добавлен пример сортировки выбором

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void SortSelection(int[] collection)
+{
+    int size = collection.Length;
+
+    for (int i = 0; i < size; i ++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if(collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+}
+
+int[] array = {7, 6, 3, 4, 5, 1, 2, 3 };
+Console.Write($"[{String.Join(',', array)}]");
+Console.WriteLine();
+SortSelection(array);
+
+Console.Write($"[{String.Join(',', array)}]");
