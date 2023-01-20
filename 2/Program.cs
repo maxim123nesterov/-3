ee007eef4da320c9b8e7183931d97a2fa4a40d4e
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2023-01-20 11:58:07 +0300

    создание алгоритма пузырьковой сортировки

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; 
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значения массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < n; i++)    // Пузырьковая сортировка
+{
+    for (int j = 0; j < n - 1; j ++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i  + ") - [" + string.Join(", ", array) + "]");
+}
